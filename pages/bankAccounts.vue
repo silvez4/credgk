1d77157de2622251db895040145c7cc928ade5d8
Gabriel_Silva <gabriel.ieda@hotmail.com>
2023-01-09 04:58:19 -0300

    PopUP de compra e validação de campos

--- pages/bankAccounts.vue
+++ pages/bankAccounts.vue
@@ -18,9 +18,138 @@
       
 <template>
   <v-row justify="space-around" class="mt-6">
-    <v-btn class="elevation-1" color="primary" @click="addPurchase">Adicionar Compra</v-btn>
-    <v-btn class="elevation-1" color="indigo" @click="addPurchase">Adicionar Deposito</v-btn>
-    <v-btn class="elevation-1" color="success" @click="addPurchase">Pagar Fatura</v-btn>
+      <v-dialog
+      transition="dialog-bottom-transition"
+      persistent
+      v-model="dialog"
+      max-width="70%"
+      min-width="500px"
+    >
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn class="elevation-1" color="primary" 
+        v-bind="attrs"
+        v-on="on"
+        >Adicionar Compra</v-btn>
+    
+    </template>
+    <v-card>
+        <v-card-title class="text-h5 justify-center">
+          Adicionar Comprar com o Cartão
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-col>
+              <v-text-field
+                    label="Nome da Transação"
+                    prepend-icon="mdi-rename-box-outline"
+                    v-model="nameTransaction"
+                    required
+                  ></v-text-field>
+            </v-col>
+            <v-col
+              >
+                <v-select
+                  :items="['Crédito', 'Débito']"
+                  prepend-icon="mdi-credit-card-multiple-outline"
+                  v-model="typeTransaction"
+                  label="Tipo de Transação"
+                  required
+                ></v-select>
+              </v-col>
+
+            <v-col>
+              <v-menu
+                ref="menu"
+                v-model="menu"
+                :close-on-content-click="false"
+                transition="scale-transition"
+                offset-y
+                max-width="290px"
+                min-width="auto"
+              >
+              <template v-slot:activator="{ on, attrs }">
+                <v-text-field
+                  v-model="dateTransaction"
+                  label="Data de Compra"
+                  hint="DD/MM/YYYY"
+                  persistent-hint
+                  prepend-icon="mdi-calendar"
+                  v-bind="attrs"
+                  v-on="on"
+                />
+              </template>
+              <v-date-picker
+                v-model="datePickerVal"
+                show-current="false"
+                locale="pt-br"
+                no-title
+                color="green lighten-1"
+                @input="menu = false; dateTransaction = parseDate(datePickerVal); checkFields()"
+              />
+            </v-menu>
+            </v-col>
+
+            <v-row>
+              <v-col cols="3" sm="3" xs="3">
+                <v-text-field
+                  v-model="qtdParcels"
+                  label="Parcelas"
+                  type="numbers"
+                  prepend-icon="mdi-cash"
+                />
+              </v-col>
+
+              <v-col cols="6" sm="4" xs="4">
+                <v-text-field
+                  v-model="valorTransaction"
+                  label="Valor Total"
+                  prefix="R$"
+                  @click="valorTransaction = ''; valParcels = ''"
+                  hint="Separe Apenas os Centavos"
+                  @blur="parseMoney(valorTransaction)"
+                  persistent-hint
+                  required
+                />
+              </v-col>
+
+              <v-col cols="6" sm="4" xs="4">
+                <v-text-field
+                  v-model="valParcels"
+                  prefix="R$"
+                  label="Valor Parcela"
+                  readonly
+                />
+              </v-col>
+            </v-row>
+            
+            <v-col class="red--text text-center">
+              <h4>Preencha Todos os Campos Antes de Salvar</h4>
+            </v-col>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="red darken-1"
+            text
+            @click="dialog = false; resetValues()"
+          >
+            Cancelar
+          </v-btn>
+          <v-btn
+            color="green darken-1"
+            text
+            :disabled="fieldsHasNoData"
+            @click="addTansaction()"
+          >
+            Adicionar
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    
+    <v-btn class="elevation-1" color="indigo" @click="addFunds()">Adicionar Deposito</v-btn>
+    <v-btn class="elevation-1" color="success" @click="payBill()">Pagar Fatura</v-btn>
   </v-row>
     <v-data-table
       :headers="headers"
@@ -80,7 +209,19 @@
 export default {
   name: 'InspirePage',
   middleware: 'auth',
+
   data: () =>({
+    nameTransaction: '',
+    typeTransaction: '',
+    valorTransaction: '',
+    dateTransaction: '',
+    qtdParcels: '',
+    valParcels: '',
+    fieldsHasNoData: true,
+    menu: false,
+    datePickerVal: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+    dialog: false,
+    newTansactions: [],
     cards: [
       {
         "title": "Total em Conta",
@@ -149,6 +290,27 @@ export default {
   mounted() {
     this.checkDif()
   },
+
+  watch: {
+    nameTransaction(val,oldval){
+      this.checkFields()
+    },
+
+    typeTransaction(val,oldval){
+      this.checkFields()
+    },
+    dateTransaction(val,oldval){
+      this.checkFields()
+    },
+    valorTransaction(val,oldval){
+      this.checkFields()
+      this.calculateParcel()
+    },
+    qtdParcels(val,oldval){
+      this.checkFields()
+      this.calculateParcel()
+    },
+  },
   computed: {
     headers () {
         return [
@@ -190,6 +352,68 @@ export default {
     },
 
   methods: {
+    resetValues() {
+      console.log(parseFloat(this.valorTransaction).toLocaleString('pt-br', {minimumFractionDigits: 2})
+      // x = Date().toLocaleString("en-GB")
+      )
+      this.nameTransaction = ''
+      this.typeTransaction = ''
+      this.valorTransaction = ''
+      this.qtdParcels = ''
+      this.valParcels = ''
+      return
+    },
+
+    parseDate (date) {
+        if (!date) return null
+
+        const [year, month, day] = date.split('-')
+        return `${day}-${month}-${year}`
+      },
+    parseMoney (money) {
+        if (!money) return null
+
+        let regex = /^[0-9]+(,[0-9]|.[0-9]+)*$/
+        let CorrectFormat = regex.test(money)
+        if(CorrectFormat){
+          let moneyConverted = parseFloat(money).toLocaleString('pt-br', {minimumFractionDigits: 2})
+          this.valorTransaction = moneyConverted
+          // console.log(this.valorTransaction)
+          return 
+        }
+        else{
+          this.valorTransaction = ''
+          this.valParcels = ''
+          return
+        }
+        return
+      },
+
+    checkFields(){
+      console.log("av")
+      if(this.nameTransaction != '' && this.typeTransaction != '' && this.dateTransaction != '' && this.valorTransaction != '' && this.qtdParcels != ''){
+        this.fieldsHasNoData = false
+      }
+      else{
+        this.fieldsHasNoData = true
+      }
+    },
+
+    addTansaction() {
+      // if(this.nameTransaction == '' || this.typeTransaction == '' || this.dateTransaction == '' || this.valorTransaction == ''){
+      //   this.fieldsHasNoData = true
+      //   return
+      // }
+      this.newTansactions.push({
+        'nome': this.nameTransaction,
+        'tipo': this.typeTransaction,
+        'data': this.dateTransaction,
+        'parcelas': this.qtdParcels,
+        'valor': this.valorTransaction,
+        'valorParcels': this.valParcels
+      })
+    },
+
     filterOnlyLowerText (value, search, item) {
         return value != null &&
           search != null &&
@@ -213,6 +437,15 @@ export default {
         }
       return this.cards[index].value < 0 ? 'card-title red--text' : 'card-title blue--text'
     },
+
+    calculateParcel() {
+      if(this.valorTransaction != '' && this.qtdParcels != ''){
+        let total = parseFloat(this.valorTransaction.replaceAll('.',''))
+        console.log(total)
+        this.valParcels = parseFloat(total / parseFloat(this.qtdParcels)).toLocaleString('pt-br', {minimumFractionDigits: 2})
+      }
+    },
+
 
     checkDif() {
       // let regex = /[+-]?\d+(\.\d+)?/g;
